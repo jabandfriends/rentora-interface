2c2db848068e6f83f7b14770f6031b22d3acfb4a
Chanathip <Chanathip24>
2025-08-20 23:30:11 +0700

    fix : corrected bg color of badge

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -8,11 +8,10 @@ const badgeVariants = tv({
   base: 'focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-theme-primary/20 dark:aria-invalid:ring-theme-primary/40 aria-invalid:border-destructive text-body-4 duration-400 inline-flex w-fit shrink-0 items-center justify-center gap-1 overflow-hidden whitespace-nowrap rounded-md border px-2 py-0.5 font-medium focus-visible:ring-[3px] [&>svg]:pointer-events-none [&>svg]:size-3',
   variants: {
     variant: {
-      default: 'bg-theme-primary text-theme-white [a&]:hover:bg-theme-primary/90 border-transparent',
-      secondary: 'bg-theme-secondary text-theme-white [a&]:hover:bg-theme-secondary/90 border-transparent',
-      success: 'bg-theme-success text-theme-white [a&]:hover:bg-theme-success/90 border-transparent',
-      error: 'bg-theme-error text-theme-white [a&]:hover:bg-theme-error/90 border-transparent',
-      warning: 'bg-theme-warning text-theme-white [a&]:hover:bg-theme-warning/90 border-transparent',
+      default: 'bg-theme-primary-50 text-theme-primary-500 [a&]:hover:bg-theme-primary/90 border-transparent',
+      success: 'bg-theme-success-50 text-theme-success-500 [a&]:hover:bg-theme-success/90 border-transparent',
+      error: 'bg-theme-error-50 text-theme-error-500 [a&]:hover:bg-theme-error/90 border-transparent',
+      warning: 'bg-theme-warning-50 text-theme-warning-500 [a&]:hover:bg-theme-warning/90 border-transparent',
     },
   },
   defaultVariants: {
