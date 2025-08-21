7e82cedc232c5dfdc05d62e51de1748a1977933b
Chanathip <Chanathip24>
2025-08-21 14:27:23 +0700

    feat : update sidebar css , and constant

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import { useDeviceWatcher } from '@/hooks'
@@ -9,11 +9,17 @@ import Sidebar from './Sidebar'
 
 const Layout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  // set sidebar open
+  const setSidebar = useCallback(() => {
+    setSidebarOpen(!sidebarOpen)
+  }, [sidebarOpen])
+
   useDeviceWatcher()
   return (
     <div className="relative min-h-screen">
-      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-      <NavBar onSidebarToggle={() => setSidebarOpen(!sidebarOpen)} />
+      <Sidebar isOpen={sidebarOpen} onClose={setSidebar} />
+      <NavBar onSidebarToggle={setSidebar} />
       <OutletWrapper>
         <Outlet />
       </OutletWrapper>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,15 +1,10 @@
-import { ChevronDown, FileMinus2, Home, User, X } from 'lucide-react'
+import { ChevronDown, User, X } from 'lucide-react'
 import { NavLink } from 'react-router-dom'
 
-import { ROUTES } from '@/constants'
+import { menuItems } from '@/constants'
 import { cn } from '@/utilities'
 
 const Sidebar = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
-  const menuItems = [
-    { icon: Home, label: 'Dashboard', href: ROUTES.home.path },
-    { icon: FileMinus2, label: 'Invoices', href: ROUTES.normalInvoice.path },
-  ]
-
   return (
     <>
       {/* Mobile overlay */}
@@ -17,65 +12,63 @@ const Sidebar = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) 
 
       {/* Sidebar */}
       <div
-        className={cn(`bg-theme-white fixed inset-y-0 left-0 z-50 w-64 transform duration-300 lg:static`, [
+        className={cn(`bg-theme-white fixed inset-y-0 left-0 z-50 flex w-64 flex-col gap-y-2 duration-300 lg:static`, [
           isOpen ? 'translate-x-0' : '-translate-x-full',
         ])}
       >
         {/* Sidebar Header */}
         <div className="flex h-16 items-center justify-between px-6">
           <div className="flex items-center gap-x-3">
-            <div className="bg-theme-primary-600 flex h-8 w-8 items-center justify-center rounded-lg">
-              <span className="text-theme-white text-heading-4">A</span>
+            <div className="bg-theme-primary-600 flex size-8 items-center justify-center rounded-lg">
+              <span className="text-theme-white text-heading-4">R</span>
             </div>
-            <span className="text-heading-4 text-theme-night-50">Rentora</span>
+            <h4 className="text-theme-night-50">Rentora</h4>
           </div>
           <button
             className="hover:bg-theme-night-800 hover:text-theme-night-50 text-theme-night-400 cursor-pointer rounded-md p-2 lg:hidden"
             onClick={onClose}
           >
-            <X className="h-5 w-5" />
+            <X className="size-5" />
           </button>
         </div>
 
         {/* Navigation */}
-        <nav className="mt-6 px-4">
+        <nav className="px-4">
           <ul className="space-y-2">
             {menuItems.map((item, index) => {
-              const Icon = item.icon
               return (
-                <li key={index}>
-                  <NavLink
-                    to={item.href}
-                    onClick={onClose}
-                    className={({ isActive }) =>
-                      cn(
-                        'text-body-2 flex items-center rounded-lg px-4 py-3 duration-200',
-                        isActive
-                          ? 'border-theme-primary-600 bg-theme-primary-50 text-theme-primary-700 border-r-2'
-                          : 'hover:bg-theme-gray-900 text-theme-gray-50 hover:text-theme-gray-50',
-                      )
-                    }
-                  >
-                    <Icon className="mr-3 h-5 w-5" />
-                    {item.label}
-                  </NavLink>
-                </li>
+                <NavLink
+                  key={'sidebar-item-' + index}
+                  to={item.to}
+                  onClick={onClose}
+                  className={({ isActive }) =>
+                    cn(
+                      'text-body-2 flex items-center rounded-lg px-4 py-3 duration-200',
+                      isActive
+                        ? 'border-theme-primary-600 bg-theme-primary-50 text-theme-primary-700 border-r-2'
+                        : 'hover:bg-theme-night-800/20 text-theme-night-50 hover:text-theme-gray-50',
+                    )
+                  }
+                >
+                  {item.icon}
+                  {item.label}
+                </NavLink>
               )
             })}
           </ul>
         </nav>
 
         {/* User section at bottom */}
-        <div className="bg-theme-gray-900/30 absolute bottom-0 left-0 right-0 border-t border-gray-600 p-4">
-          <div className="flex items-center">
-            <div className="bg-theme-gray-50 flex h-8 w-8 items-center justify-center rounded-full">
-              <User className="text-theme-gray-50 h-4 w-4" />
+        <div className="border-theme-night-500 absolute bottom-0 left-0 right-0 border-t p-4">
+          <div className="flex items-center gap-x-2">
+            <div className="flex size-8 items-center justify-center rounded-full border">
+              <User className="text-theme-gray-50 size-4" />
             </div>
-            <div className="ml-3 flex-1">
-              <p className="text-theme-gray-50 text-body-2">John Doe</p>
-              <p className="text-theme-gray-50 text-body-3">john@example.com</p>
+            <div className="text-theme-night-300">
+              <p className="text-body-2">John Doe</p>
+              <p className="text-body-3">john@example.com</p>
             </div>
-            <ChevronDown className="text-theme-gray-50 h-4 w-4" />
+            <ChevronDown className="text-theme-gray-50 size-4" />
           </div>
         </div>
       </div>
