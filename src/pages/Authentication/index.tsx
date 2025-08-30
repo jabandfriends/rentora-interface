ef8b9db5f3e2a1f729772ca448f3d202397926b6
Midnight4002 <mercury2547@icloud.com>
2025-08-31 00:12:02 +0700

    fix : delete border from login page

--- src/pages/Authentication/index.tsx
+++ src/pages/Authentication/index.tsx
@@ -2,7 +2,7 @@ import { AuthenticationBody } from '@/components/pages/Authentication'
 
 const Authentication = () => {
   return (
-    <div className="flex size-full h-dvh items-center justify-center border">
+    <div className="flex size-full h-dvh items-center justify-center">
       <AuthenticationBody />
     </div>
   )
