c5e1b4ca4032f90c19fb46ca070dadca801ccb37
octovir <octovir@gmail.com>
2025-08-31 12:18:56 +0700

    feat : update electric water report

--- src/components/pages/Report/ElectricWaterReport/ElectricWaterReport.tsx
+++ src/components/pages/Report/ElectricWaterReport/ElectricWaterReport.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
 function ElectricWaterReport() {
-  return <div>ElectricWaterReport</div>
+    return <div>ElectricWaterReport</div>
 }
 
 export default ElectricWaterReport
