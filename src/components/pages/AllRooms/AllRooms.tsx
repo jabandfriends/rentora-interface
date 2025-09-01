54e3c2e455ca90e3bd34a20e16dba67afd14e28c
Chanathip <chanathip.cha@outlook.com>
2025-09-01 10:09:46 +0700

    feat : add component Image, env

--- src/components/pages/AllRooms/AllRooms.tsx
+++ src/components/pages/AllRooms/AllRooms.tsx
@@ -3,13 +3,13 @@ import { PageTableHeader, PageTableSearch } from '@/components/ui'
 import { ALL_ROOMS_DATA, All_ROOMS_STAT } from '@/constants'
 
 const AllRooms = () => {
-    return (
-        <>
-            <PageTableHeader title="All Rooms" description="All rooms with category dashboard" stats={All_ROOMS_STAT} />
-            <PageTableSearch />
-            <AllRoomsTable data={ALL_ROOMS_DATA} />
-        </>
-    )
+  return (
+    <>
+      <PageTableHeader title="All Rooms" description="All rooms with category dashboard" stats={All_ROOMS_STAT} />
+      <PageTableSearch />
+      <AllRoomsTable data={ALL_ROOMS_DATA} />
+    </>
+  )
 }
 
 export default AllRooms
