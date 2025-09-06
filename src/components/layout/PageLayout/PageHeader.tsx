436874d8bca097b1002bdba35521b15c587266de
Chanathip24 <160759163+Chanathip24@users.noreply.github.com>
2025-09-06 22:35:35 +0700

    refactor : page header page header

--- src/components/layout/PageLayout/PageHeader.tsx
+++ src/components/layout/PageLayout/PageHeader.tsx
@@ -1,17 +1,27 @@
 import { type ReactNode } from 'react'
 
+import { Button } from '@/components/common'
+
 type IPageHeaderProps = {
   title: string
   description: string
-  actionButton?: ReactNode
+  isAction?: boolean
+  actionLabel?: string
+  actionIcon?: ReactNode
+  actionOnClick?: () => void
 }
 
-const PageHeader = ({ title, description, actionButton }: IPageHeaderProps) => {
+const PageHeader = ({ title, description, isAction, actionLabel, actionIcon, actionOnClick }: IPageHeaderProps) => {
   return (
     <div className="flex flex-col gap-1">
       <div className="flex items-center justify-between">
         <h2>{title}</h2>
-        {actionButton}
+        {isAction && (
+          <Button className="flex items-center gap-2" onClick={actionOnClick}>
+            {actionIcon}
+            {actionLabel}
+          </Button>
+        )}
       </div>
 
       <p className="text-theme-secondary">{description}</p>
