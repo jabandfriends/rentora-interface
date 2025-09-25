72be6222a07ce15c45448e3f5745e02e9d367cc0
octovir <octovir@gmail.com>
2025-09-26 01:03:45 +0700

    feat : update comment for create maintenance in maintenance hook type

--- src/types/hooks/api/execute/maintenance.ts
+++ src/types/hooks/api/execute/maintenance.ts
@@ -2,7 +2,7 @@ import type { Category, Priority, RecurringSchedule, Status } from '@/enum'
 
 import type { IBaseUseMutation, IRentoraApiClientBaseResponse } from '../base-api'
 
-//payload type
+//create payload type
 type ICreateMaintenanceRequestBase = {
   unitId: string
   tenantUserId: string
@@ -25,15 +25,16 @@ type ICreateMaintenanceRequestBase = {
   recurringSchedule: RecurringSchedule
 }
 
+//create payload type
 export type ICreateMaintenanceRequestPayload = ICreateMaintenanceRequestBase
 
-//hook type
+//create hook type
 export type IUseRentoraApiCreateMaintenance = IBaseUseMutation<
   IRentoraApiClientCreateMaintenanceResponse['data'],
   { apartmentId: string; payload: ICreateMaintenanceRequestPayload }
 >
 
-//response type
+//create response type
 export type IRentoraApiClientCreateMaintenanceResponse = IRentoraApiClientBaseResponse<{
   maintenanceId: string
 }>
