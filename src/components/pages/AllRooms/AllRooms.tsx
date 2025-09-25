921a50f15dccfb2e99687b36b846982f970fe7c2
Chanathip24 <160759163+Chanathip24@users.noreply.github.com>
2025-09-25 19:50:32 +0700

    fix : commented pagetable search from unconnected api page

--- src/components/pages/AllRooms/AllRooms.tsx
+++ src/components/pages/AllRooms/AllRooms.tsx
@@ -1,12 +1,12 @@
 import AllRoomsTable from '@/components/pages/AllRooms/AllRoomsTable'
-import { PageTableHeader, PageTableSearch } from '@/components/ui'
+import { PageTableHeader } from '@/components/ui'
 import { ALL_ROOMS_DATA, All_ROOMS_STAT } from '@/constants'
 
 const AllRooms = () => {
   return (
     <>
       <PageTableHeader title="All Rooms" description="All rooms with category dashboard" stats={All_ROOMS_STAT} />
-      <PageTableSearch />
+      {/* <PageTableSearch /> */}
       <AllRoomsTable data={ALL_ROOMS_DATA} />
     </>
   )
