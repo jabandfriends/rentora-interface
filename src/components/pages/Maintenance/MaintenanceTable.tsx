933e0ec2e5d5cca5b066636d55c02fbbc3ddeb15
octovir <octovir@gmail.com>
2025-10-07 16:22:39 +0700

    feat : add dropdown update and delete

--- src/components/pages/Maintenance/MaintenanceTable.tsx
+++ src/components/pages/Maintenance/MaintenanceTable.tsx
@@ -4,6 +4,8 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { Badge, PaginationBar, Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui'
 import { MAINTENANCE_TABLE_HEADER, ROUTES } from '@/constants'
 
+import MaintenanceAction from './MaintenanceAction'
+
 type IMaintenanceTableProps = {
   data: Array<any>
 }
@@ -11,6 +13,14 @@ type IMaintenanceTableProps = {
 const MaintenanceTable = ({ data }: IMaintenanceTableProps) => {
   const navigate = useNavigate()
   const { apartmentId } = useParams<{ apartmentId: string }>()
+
+  const handleUpdateTenant = useCallback(
+    (tenantId: string) => {
+      if (!apartmentId) return
+      navigate(ROUTES.tenantUpdate.getPath(apartmentId, tenantId))
+    },
+    [apartmentId, navigate],
+  )
 
   const handleRowClick = useCallback(
     (index: string) => {
@@ -31,7 +41,7 @@ const MaintenanceTable = ({ data }: IMaintenanceTableProps) => {
         </TableHeader>
         <TableBody>
           {data.map((item, index) => (
-            <TableRow className="cursor-pointer" onClick={() => handleRowClick(index.toString())} key={index}>
+            <TableRow className="cursor-pointer" onClick={() => handleRowClick(item.maintenanceId)} key={index}>
               <TableCell>{item.room}</TableCell>
               <TableCell>{item.buildings}</TableCell>
               <TableCell>{item.issuesDate} </TableCell>
@@ -39,6 +49,19 @@ const MaintenanceTable = ({ data }: IMaintenanceTableProps) => {
               <TableCell>{item.servicerequest}</TableCell>
               <TableCell>
                 <Badge variant={item.type}>{item.status}</Badge>
+              </TableCell>
+              <TableCell>
+                <div
+                  onClick={(e) => {
+                    e.stopPropagation()
+                  }}
+                >
+                  <MaintenanceAction
+                    maintenanceId={item.maintenanceId}
+                    onUpdate={handleUpdateTenant}
+                    onDelete={handleUpdateTenant}
+                  />
+                </div>
               </TableCell>
             </TableRow>
           ))}
