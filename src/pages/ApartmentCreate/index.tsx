035ebde355057ececfb15d3cf86369006bdf5b5c
Chanathip24 <160759163+Chanathip24@users.noreply.github.com>
2025-10-14 21:09:50 +0700

    fix:use page section with apartment page

--- src/pages/ApartmentCreate/index.tsx
+++ src/pages/ApartmentCreate/index.tsx
@@ -1,11 +1,29 @@
-import { ApartmentCreateForm, ApartmentCreateHeader } from '@/components/pages/ApartmentCreate'
+import { ArrowLeftIcon } from 'lucide-react'
+import { useCallback } from 'react'
+import { type NavigateFunction, useNavigate } from 'react-router-dom'
+
+import { PageHeader, PageSection } from '@/components/layout'
+import { ApartmentCreateForm } from '@/components/pages/ApartmentCreate'
+import { ROUTES } from '@/constants'
 
 const ApartmentCreatePage = () => {
+  const navigate: NavigateFunction = useNavigate()
+
+  const handleNavigateApartment = useCallback(() => {
+    navigate(ROUTES.allApartment.path)
+  }, [navigate])
   return (
-    <div className="container mx-auto flex flex-col gap-y-5 py-5">
-      <ApartmentCreateHeader />
+    <PageSection middle>
+      <PageHeader
+        title="Add New Apartment"
+        description="Fill in the apartment details to start managing your property."
+        isAction
+        actionLabel="Back"
+        actionIcon={<ArrowLeftIcon />}
+        actionOnClick={handleNavigateApartment}
+      />
       <ApartmentCreateForm />
-    </div>
+    </PageSection>
   )
 }
 
