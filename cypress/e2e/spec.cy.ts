af4640845f39fcaa6f50dd8ec9253b179748a13c
Chanathip24 <160759163+Chanathip24@users.noreply.github.com>
2025-10-17 19:58:03 +0700

    fix : remove spec cy

--- cypress/e2e/spec.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('template spec', () => {
-  it('visits the example page', () => {
-    cy.visit('https://example.cypress.io')
-    cy.contains('type').should('exist') // example assertion
-  })
-})
