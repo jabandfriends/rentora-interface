28a399fc1db31c95deff4d239a2aac837870a514
Chanathip <chanathip.cha@outlook.com>
2025-10-18 11:43:49 +0700

    fix : base url

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,2 +1,4 @@
 export const RENTORA_API_BASE_URL =
-  window.__RENTORA_API_BASE_URL__ || import.meta.env.VITE_RENTORA_API_BASE_URL || 'http://localhost:8099'
+  import.meta.env.MODE === 'production'
+    ? (window as any).__RENTORA_API_BASE_URL__ || import.meta.env.VITE_RENTORA_API_BASE_URL
+    : import.meta.env.VITE_RENTORA_API_BASE_URL || 'http://localhost:8099'
