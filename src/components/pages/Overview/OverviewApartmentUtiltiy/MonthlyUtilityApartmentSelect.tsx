c93df30b19c90add33152a8d723d7f60c6deb072
ichachiraya <anakbanpotkul@gmail.com>
2025-11-09 08:34:06 +0700

    refactor: improve MonthlyUtilityApartmentSelect component

--- src/components/pages/Overview/OverviewApartmentUtiltiy/MonthlyUtilityApartmentSelect.tsx
+++ src/components/pages/Overview/OverviewApartmentUtiltiy/MonthlyUtilityApartmentSelect.tsx
@@ -1,48 +1,50 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@radix-ui/react-select'
-import { type Dispatch, type SetStateAction, useState } from 'react'
+import { type Dispatch, type SetStateAction, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { number } from 'zod'
 
 import { LoadingPage, PageTableEmpty } from '@/components/ui'
-import { useRentoraApiApartmetUtility, useRentoraApiApartmetUtiliyy } from '@/hooks'
-import { useRentoraApiYearlyApartmentUtility } from '@/hooks/api/queries/useRentoraApiYearlyApartmentUtility'
-import type { IApartmentUtility, IYearlyApartmentUtility } from '@/types'
+import { useRentoraApiApartmetUtility } from '@/hooks'
+import type { IYearlyApartmentUtility } from '@/types'
 
 import MonthlyUtilityApartmentCard from './MonthlyUtilityApartmentCard'
 
-type IYearlySelect = {
+type IYearlySelectProps = {
     item?: Array<IYearlyApartmentUtility>
     isLoading: boolean
 }
 
-const MonthlyUtilityApartmentSelect = ({ item, isLoading }: IYearlySelect) => {
+const MonthlyUtilityApartmentSelect = ({ item, isLoading }: IYearlySelectProps) => {
+    const { apartmentId } = useParams<{ apartmentId: string }>()
+
     const [selectedYear, setSelectedYear]: [string, Dispatch<SetStateAction<string>>] = useState('')
 
-    const { apartmentId } = useParams<{ apartmentId: string }>()
-    const { data: monthlyApartmentUtility, isLoading: boolean } = useRentoraApiApartmetUtility({
+    useEffect(() => {
+        if (item && item.length > 0 && selectedYear === '') {
+            setSelectedYear(String(item[0].year))
+        }
+    }, [item, selectedYear])
+
+    const { data: monthlySummary, isLoading: isLoadingSummary } = useRentoraApiApartmetUtility({
         apartmentId: apartmentId!,
         params: {
-            year: selectedYear,
+            year: Number(selectedYear),
         },
     })
-    if (isLoading) {
+
+    if (isLoading || isLoadingSummary) {
         return <LoadingPage />
     }
 
     if (!item || item.length === 0) {
-        return <PageTableEmpty message="No Floor found" />
-    }
-
-    if (selectedYear === '' && item.length > 0) {
-        setSelectedYear(String(item[0].year))
+        return <PageTableEmpty message="No historical data found to select a year." />
     }
 
     return (
         <div className="justify-center space-y-4 rounded-2xl">
             <div>
                 <Select value={selectedYear} onValueChange={(value) => setSelectedYear(value)}>
-                    <SelectTrigger>
-                        <SelectValue placeholder="Select floor Number" />
+                    <SelectTrigger className="w-[150px]">
+                        <SelectValue placeholder="Select Year" />
                     </SelectTrigger>
                     <SelectContent>
                         {item.map((yearSummary: IYearlyApartmentUtility) => (
@@ -55,11 +57,16 @@ const MonthlyUtilityApartmentSelect = ({ item, isLoading }: IYearlySelect) => {
                     </SelectContent>
                 </Select>
             </div>
-            <div>
-                {monthlyApartmentUtility?.map((item: IApartmentUtility, index: number) => (
-                    <MonthlyUtilityApartmentCard key={index} item={item} isLoading={isLoading} props={number(selectedYear)} />
-                ))}
-            </div>
+
+            {monthlySummary && selectedYear && (
+                <MonthlyUtilityApartmentCard
+                    item={monthlySummary}
+                    isLoading={isLoadingSummary}
+                    props={{
+                        year: Number(selectedYear),
+                    }}
+                />
+            )}
         </div>
     )
 }
